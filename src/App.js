6e738bece468a75b458b0317808fba4e974bdc35
imskanand <the.anand3099@gmail.com>
2022-06-14 21:38:13 +0530

    Basic Structure of the App is completed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
+import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import { Box } from '@mui/material'
+import NavBar from './components/NavBar'
+import Home from './pages/Home'
+import ExerciseDetail from './pages/ExerciseDetail'
+import Footer from './components/Footer'
+
 function App() {
-  return <div>Hello , Fit Expert!!
-  </div>;
+  return <Box width="400" sx={ { width: { xl: '1488px' } } } m='auto' >
+    <NavBar />
+    <Routes>
+      <Route path='/' element={ <Home /> } />
+      <Route path='/exercise/:id' element={ <ExerciseDetail /> } />
+    </Routes>
+    <Footer/>
+  </Box>
 
 }
 export default App;
