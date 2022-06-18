09b59548eb87e78169ff92a40605489a8ac7ef9e
imskanand <the.anand3099@gmail.com>
2022-06-18 12:25:06 +0530

    Home Page is Done

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -4,19 +4,33 @@ import {Pagination} from '@mui/material/Pagination'
 import {exerciseOptions, fetchData} from '../utils/fetchData'
 import ExerciseCard from './ExerciseCard.js'
 
-const Exercises = ({exercises, setExercises, bodyParts}) => {
+const Exercises = ({exercises, setExercises, bodyPart}) => {
   const [currentPage, setCurrentPage] = useState(1)
   const exercisesPerPage = 9
 
   const indexOfLastExercise = currentPage * exercisesPerPage
   const indexOfFirstExercise = indexOfLastExercise - exercisesPerPage
-  const currentExercises = exercises.slice(indexOfFirstExercise,indexOfLastExercise)
+  const currentExercises = exercises.slice(indexOfFirstExercise, indexOfLastExercise)
 
   const paginate = (e, value) => {
     setCurrentPage(value)
 
     window.scrollTo({top: 1800, behavior: 'smooth'})
   }
+
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      let exercisesData = []
+      if (bodyPart === 'all') {
+        exercisesData = await fetchData('https://exercisedb.p.rapidapi.com/exercises', exerciseOptions)
+      } else {
+        exercisesData = await fetchData(`https://exercisedb.p.rapidapi.com/exercises/bodyPart/${bodyPart}`, exerciseOptions)
+      }
+      setExercises(exercisesData)
+    }
+    fetchExercisesData()
+  }, [bodyPart])
+
   return (
     <Box id='exercises'
       sx={{mt: {lg: '110px'}}}
