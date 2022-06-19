12906d4b9d836d07c908409aca58a39117e89b91
imskanand <the.anand3099@gmail.com>
2022-06-19 09:03:05 +0530

    created exercise details components

--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -1,4 +1,16 @@
+import {useEffect, useState} from 'react'
+import {useParams} from 'react-router-dom'
+import {Box} from '@mui/material'
+import {exerciseOptions, fetchData} from '../utils/fetchData'
+import SimilarExercises from '../components/SimilarExercises'
+import ExerciseVideos from '../components/ExerciseVideos'
+import Detail from '../components/Detail'
+
 const ExerciseDetail = () => {
-  return <div>ExerciseDetail</div>;
-};
-export default ExerciseDetail;
+  return <Box>
+    <Detail />
+    <ExerciseVideos />
+    <SimilarExercises />
+  </Box>
+}
+export default ExerciseDetail
